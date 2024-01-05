fde929228cdb0acbdc6c60ef4f4fe6b39c569967
Mageto-Nyakoni <91837203+Mageto-Nyakoni@users.noreply.github.com>
2024-01-05 00:55:47 -0600

    Initial Commit

---
+++ PreworkSP24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PreworkSP24
+//
+//  Created by Mageto Nyakoni on 1/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
